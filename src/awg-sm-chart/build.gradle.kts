59e792b50378ba19fe334c0bfb7f0b244123e443
Jerome Haltom <jhaltom@alethic.solutions>
2025-05-09 12:15:44 -0500

    Hmm.

--- src/awg-sm-chart/build.gradle.kts
+++ src/awg-sm-chart/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
     id("com.citi.helm") version "2.2.0"
-    id("com.palantir.git-version") version "3.2.0"
 }
 
-val gitVersion: groovy.lang.Closure<String> by extra
-version = gitVersion()
 description = "awg-sm-chart"
